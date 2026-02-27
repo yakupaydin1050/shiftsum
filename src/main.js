cadfa8bc2e230b397e4ca09032211a6002ceca85
Yakup Aydin <85905113+yakupaydin1050@users.noreply.github.com>
2026-02-27 17:31:49 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+import { GridManager } from "./core/gridManager.js";
+import { Renderer } from "./ui/renderer.js";
+import { InputController } from "./ui/inputController.js";
 
+const gridManager = new GridManager(5);
+const renderer = new Renderer("game");
+
+renderer.render(gridManager.grid);
+
+new InputController((direction) => {
+  const moved = gridManager.moveEmpty(direction);
+  if (moved) {
+    renderer.render(gridManager.grid);
+  }
+});
