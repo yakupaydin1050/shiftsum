422747f72badf284d78187201be8d2389fa69d04
Yakup Aydin <85905113+yakupaydin1050@users.noreply.github.com>
2026-02-27 17:31:37 +0100

    Update inputController.js

--- src/ui/inputController.js
+++ src/ui/inputController.js
@@ -1,1 +1,11 @@
-
+export class InputController {
+  constructor(onMove) {
+    document.addEventListener("keydown", (e) => {
+      if (
+        ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key)
+      ) {
+        onMove(e.key);
+      }
+    });
+  }
+}
